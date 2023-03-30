5d3b65ccf5dc1fc03590a0e00598fbc1927fcefe
Md.Abdullah al mamun <mamun1214@gmail.com>
2023-03-30 09:22:10 +0600

    Fetch data from API

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,16 @@
+/** ****************************************
+ * Project: math-magicians
+ * File: LoadingSpinner.js
+ * Created: 3/30/23
+ * Author: Abdullah Al Mamun <mamun1214@gmail.com>
+ ****************************************** */
+import React from 'react';
+import '../css/spinner.css';
+
+export default function LoadingSpinner() {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner" />
+    </div>
+  );
+}
